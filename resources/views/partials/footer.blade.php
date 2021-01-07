2fe6995ad49cf2c578f0c92a96ea63c4d6609f47
Annalisa <annalisadesantis770@gmail.com>
2021-01-07 16:42:18 +0100

    footer-bottom

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,38 @@
 <footer>
-    
+    <div class="footer-top">
+        <div class="container">
+
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom-box">
+                <div class="bottone">
+                    <button type="button" name="button" class="uppercase">
+                        Sign-up now!
+                    </button>
+                </div>
+                <div class="icone">
+                    <span class="uppercase">
+                        follow us
+                    </span>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-facebook.png')}}" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-twitter.png')}}" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-youtube.png')}}" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-pinterest.png')}}" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-periscope.png')}}" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
