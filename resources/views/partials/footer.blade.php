57afc810bf94ab74a2ed18e452e03d0bbe87d5fc
Annalisa <annalisadesantis770@gmail.com>
2021-01-07 18:05:20 +0100

    fine footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,9 +2,174 @@
     <div class="footer-top">
         <div class="footer-top-db">
             <div class="container">
+                <div class="footer-menu">
+                    <div class="voci-menu">
+                        <div class="menu">
+                            <h4>
+                                Dc Comics
+                            </h4>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Characters
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Comics
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Movies
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        TV
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Games
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Videos
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        News
+                                    </a>
+                                </li>
+                            </ul>
+                            <h4>
+                                Shop
+                            </h4>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Shop DC
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Shop DC Collectibles
+                                    </a>
+                                </li>
 
+                            </ul>
+                        </div>
+                        <div class="menu">
+                            <h4>
+                                Dc
+                            </h4>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Terms Of Use
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Privacy Policy (New)
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Ad Choices
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Advertising
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Jobs
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Subscriptions
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Talent Workshop
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        CPSC Certificates
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Ratings
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Shop Help
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Contact Us
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="menu">
+                            <h4>
+                                Sites
+                            </h4>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Dc
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        MAD Magazine
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        DC Kids
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        DC Universe
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        DC Power Visa
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cookies">
+                        <span>
+                            All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">
+                                noted here.
+                            </a> All rights reserved. <br>
+                            <a href="#">
+                                Cookies Settings
+                            </a>
+                        </span>
+                    </div>
+                </div>
             </div>
-        </div>    
+        </div>
     </div>
     <div class="footer-bottom">
         <div class="container">
