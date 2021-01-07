093a08d50f7d2d1551c251934e2ba4a34c595853
Annalisa <annalisadesantis770@gmail.com>
2021-01-07 16:16:28 +0100

    blue line

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,5 +33,51 @@
                 </div>
             </div>
         </div>
+        <div class="iconlink">
+            <div class="container">
+                <div class="iconlink-box">
+                    <a href="#">
+                        <div class="img-box">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <span class="uppercase">
+                            Digital Comics
+                        </span>
+                    </a>
+                    <a href="#">
+                        <div class="img-box">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <span class="uppercase">
+                            DC Merchandise
+                        </span>
+                    </a>
+                    <a href="#">
+                        <div class="img-box">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <span class="uppercase">
+                            Subscriptions
+                        </span>
+                    </a>
+                    <a href="#">
+                        <div class="img-box">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <span class="uppercase">
+                            Comic Shop Locator
+                        </span>
+                    </a>
+                    <a href="#">
+                        <div class="img-box">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-dc-power-visa.svg" alt="">
+                        </div>
+                        <span class="uppercase">
+                            Dc power visa
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
