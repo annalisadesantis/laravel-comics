56e8823bac075ced147f42dd71f1d2854b998a88
Annalisa <annalisadesantis770@gmail.com>
2021-01-07 15:58:11 +0100

    card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,33 @@
 @section("content")
     <section id="home">
         <div class="jumbtron">
-            {{-- <img src="{{ asset('images/jumbotron.jpg')}}" alt=""> --}}
+
+        </div>
+        <div class="comics">
+            <div class="container">
+                <small class="uppercase">
+                    Current Series
+                </small>
+                <div class="comics-container">
+                    @foreach ($comics as $comic)
+                        <div class="comic">
+                            <div class="comic-image">
+                                <a href="#">
+                                    <img src="{{ $comic['thumb']}}" alt="{{ $comic['series']}}">
+                                </a>
+                            </div>
+                            <div class="comis-title">
+                                <span class="uppercase">
+                                    {{ $comic['series']}}
+                                </span>
+                            </div>
+                        </div>
+                    @endforeach
+                    <button type="button" name="button" class="uppercase">
+                        Load more
+                    </button>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
