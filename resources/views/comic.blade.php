58b7e3444ef3223c92d44620ec524ee88a3042ae
Annalisa <annalisadesantis770@gmail.com>
2021-01-08 15:15:41 +0100

    blue line page details

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,8 +3,23 @@
 @section("page-title", "Comic-Detalis | DC")
 {{-- contenuti --}}
 @section("content")
-    <section id="comic">
-        @include("partials.jumbtron")
-        <h1>ciao</h1>
+    @include("partials.jumbtron")
+    <section class="comic-thumb">
+        <div class="small-container">
+            <div class="comic-thumb-container">
+                <div class="comic-thumb-wrapper">
+                    <span class="comic-type uppercase">
+                        {{ $comic['type'] }}
+                    </span>
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <span class="comic-gallery uppercase">
+                        View Gallery
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="comic-details">
+
     </section>
 @endsection
